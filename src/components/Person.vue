403e343150bcff265ab7ab1b6773c44d0b1f1310
duasong111 <2272168170@qq.com>
2025-03-07 16:22:09 +0800

    提交有关路由的代码进行保存

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,17 +1,12 @@
 <template>
     <div class="person">
-        <h2>现在是使用reactive</h2>
-        <hr>
-        <h4>汽车的品牌是 {{ car.brand }},汽车的价格是 {{ car.price }}</h4>
-        <button @click="changPrice">改变价格</button>
-        <hr>
-        <ul>
-            <li v-for="game in games" :key="game.id">{{ game.name }}</li>
-        </ul>
-        <button @click="changeGameName">修改游戏的名字</button>
-        <hr>
-        <h3>{{ valueData.a.b.c.data }}</h3>
-        <button @click="changeValue">修改游戏的名字</button>
+        姓: <input type="text" v-model="firstName"> <br>
+        名:<input type="text" v-model="lastName"> <br>
+        全名:<span>{{ fullName}}</span>
+        <br>
+        <button @click="changefullName">将全名改li-si</button>
+        
+
     </div>
 </template>
 <!-- <script lang="ts">
@@ -22,37 +17,24 @@ export default {
 
 <script lang="ts" setup name="Person">//使用setup的语法糖
 
-import { ref,reactive } from 'vue'
+import { ref, reactive, toRefs, computed } from 'vue'
+let firstName = ref("张")
+let lastName = ref("王")
 
-let car = reactive(({ brand: "奔驰", price: 777 }))
-
-let games = reactive([
-    { id: '001', name: "王者荣耀" },
-    { id: '002', name: "海岛奇兵" },
-    { id: '003', name: "哇哈哈" }
-])
-
-let valueData =ref({a:{
-    b:{
-        c:{
-            data:12345
-        }
+let fullName = computed({
+    get() {
+        return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' +
+            lastName.value
+    },
+    set(vla){
+        const[str1,str2]=vla.split('-')
+        firstName.value=str1
+        lastName.value=str2
     }
+})
+function changefullName() {
+    fullName.value = "li-si"
 }
-    }
-)
-
-function changPrice() {
-    car.price += 100;
-}
-function changeGameName() {
-    games[0].name = "改变名字"
-}
-function changeValue(){
-    valueData.value.a.b.c.data=3333
-} //但凡用到ref一定要切记加上value
-
-
 
 </script>
 
